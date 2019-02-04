f5509df65d74a299631c1b59d39928fbb8dd9d47
Nina Withers <ninarosewithers@gmail.com>
2019-02-04 07:53:57 -0500

    Built structure for html and connections to css, js, and firebase

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAaSPvGSdh8VxsaIQYU2_iL8AruQRUkhHs",
+	authDomain: "",
+	databaseURL: "",
+	storageBucket: "",
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
