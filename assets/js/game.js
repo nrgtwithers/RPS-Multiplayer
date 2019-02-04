2ef45bdbe37547b085e3bb8867eca4aa75e6a3f1
Nina Withers <ninarosewithers@gmail.com>
2019-02-04 09:38:51 -0500

    creating layout and adding styling via css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,26 @@
+// Viewing purposes JS
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
 // Initialize connection to Firebase
-var config = {
-	apiKey: "AIzaSyAaSPvGSdh8VxsaIQYU2_iL8AruQRUkhHs",
-	authDomain: "",
-	databaseURL: "",
-	storageBucket: "",
-};
 
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyAaSPvGSdh8VxsaIQYU2_iL8AruQRUkhHs",
+//     authDomain: "",
+//     databaseURL: "",
+//     storageBucket: "",
+// };
 
-var database = firebase.database();
+// firebase.initializeApp(config);
+
+// var database = firebase.database();
 
 // Variables (Players - 2 only, Score - Wins and Losses)
 // Allow only Two Users to play at the same time.
 // Provide the ability to choose Rock paper Scissors at the same time (On-click, Make sure its both at the same time.)
 // Players 1 & 2 (Wins and Loses)
 // Provide capability for players to speak to each other.
+
+
+
