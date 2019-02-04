8f1be9f18467bf95edfee8ecd46c7849d29871b7
Nina Withers <ninarosewithers@gmail.com>
2019-02-04 15:04:17 -0500

    added firebase connection to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,19 +10,28 @@ M.textareaAutoResize($('#textarea1'));
 
 // Initialize connection to Firebase
 
-// var config = {
-//     apiKey: "AIzaSyAaSPvGSdh8VxsaIQYU2_iL8AruQRUkhHs",
-//     authDomain: "",
-//     databaseURL: "",
-//     storageBucket: "",
-// };
+var config = {
+    apiKey: "AIzaSyAaSPvGSdh8VxsaIQYU2_iL8AruQRUkhHs",
+    authDomain: "distributed-eye-226422.firebaseapp.com",
+    databaseURL: "https://distributed-eye-226422.firebaseio.com",
+    projectId: "distributed-eye-226422",
+    storageBucket: "distributed-eye-226422.appspot.com",
+    messagingSenderId: "439695275055"
+  };
+  firebase.initializeApp(config);
 
-// firebase.initializeApp(config);
+var database = firebase.database();
 
-// var database = firebase.database();
+// Variables (Players - 2 only, Score - Wins and Losses + Ties)
+var wins = 0;
+var loses = 0;
+var ties = 0;
+var numOfPlayers = 2; // Allow only Two Users to play at the same time.
 
-// Variables (Players - 2 only, Score - Wins and Losses)
-// Allow only Two Users to play at the same time.
+var player1;
+var player2;
+
+
 // Provide the ability to choose Rock paper Scissors at the same time (On-click, Make sure its both at the same time.)
 // Players 1 & 2 (Wins and Loses)
 // Provide capability for players to speak to each other.
