e25da17bf32a080de25efa6113ff04163d5104d9
Nina Withers <ninarosewithers@gmail.com>
2019-02-04 08:26:43 -0500

    adding pseudocode

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,3 +9,9 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+// Variables (Players - 2 only, Score - Wins and Losses)
+// Allow only Two Users to play at the same time.
+// Provide the ability to choose Rock paper Scissors at the same time (On-click, Make sure its both at the same time.)
+// Players 1 & 2 (Wins and Loses)
+// Provide capability for players to speak to each other.
